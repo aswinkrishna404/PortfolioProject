aab767cc92450a32a3a3ba9b94d35ff52b2d6057
ASWIN KRISHNA <ASWIN KRISHNA@DESKTOP-38B852S>
2025-03-05 11:23:12 +0530

    initial commit

---
+++ PortfolioProject/Data/ApplicationDbcontext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PortfolioProject.Models;
+
+namespace PortfolioProject.Data
+{ 
+      public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+    { }
+    public DbSet<Contact> ContactForm { get; set; }
+
+
+
+}
+}
---
+++ PortfolioProject/Models/Entities/Contact.cs
@@ -0,0 +1,21 @@
+﻿
+
+
+ using System.ComponentModel.DataAnnotations;
+
+namespace PortfolioProject.Models
+{
+
+    public class Contact
+    {
+        [Key]
+
+        public string Name { get; set; }
+        [EmailAddress]
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public string Message { get; set; }
+
+    }
+
+}
